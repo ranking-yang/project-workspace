4b874571bce663ce2986c4103fd0f003b270d509
Leekmkm <qlqkqj12@naver.com>
2023-07-19 17:48:43 +0900

    20230719/이경민/로그인UI작업
    로그인/회원가입 UI 작업

--- WebProject/src/main/java/com/team/webproject/service/ExampleService.java
+++ WebProject/src/main/java/com/team/webproject/service/ExampleService.java
@@ -1,14 +1,11 @@
 package com.team.webproject.service;
 
-import java.util.List;
-
-import com.team.webproject.dto.Example;
 import com.team.webproject.dto.MembersDTO;
 
 public interface ExampleService {
-	List<Example> getAll();
+	
 
-	public void memberJoin(MembersDTO member) throws Exception;
+	void memberJoin(MembersDTO member) throws Exception;
 
 	int add(MembersDTO login);
 }
