1b5639bba8c72518b8a5bbf6ab201041d48f03b2
jiin3498 <a45953498@gmail.com>
2023-08-04 14:43:07 +0900

    230804 김수진 마이페이지 UI 작성중

--- WebProject/src/main/java/com/team/webproject/controller/MypageController.java
+++ WebProject/src/main/java/com/team/webproject/controller/MypageController.java
@@ -1,14 +1,7 @@
 package com.team.webproject.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.team.webproject.dto.MembersDTO;
-import com.team.webproject.service.LoginService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class MypageController {
 	
-	private final LoginService exService;
-	
 	@GetMapping("/mypage")
 	String main() {
-		return "/mypage/MyPageMain";
+		return "/mypage/mypage";
 	}
+	
+	
 }
