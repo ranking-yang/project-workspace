ffd444314fc0ab522a7f82a64d7863150152f12f
ParkJungJun <132882186+ParkJungJun@users.noreply.github.com>
2023-08-04 17:29:05 +0900

    23/08/04 박정준 리뷰 데이터베이스 연결함 왜 다가.. 하기시렁

--- WebProject/src/main/java/com/team/webproject/mapper/ReviewMapper.java
+++ WebProject/src/main/java/com/team/webproject/mapper/ReviewMapper.java
@@ -1,17 +1,14 @@
 package com.team.webproject.mapper;
 
+import java.util.List;
+
 import com.team.webproject.dto.ReviewDTO;
 
 public interface ReviewMapper {
-	// 리뷰 작성
-    int insertReview(ReviewDTO review);
-
-    // 리뷰 조회
-    ReviewDTO selectReviewByCode(Integer reviewCode);
-
-    // 리뷰 수정
-    int updateReview(ReviewDTO review);
-
-    // 리뷰 삭제
-    int deleteReview(Integer reviewCode);
+	List<ReviewDTO> getAllReviews(); // 모든 리뷰 조회
+    ReviewDTO getReviewByCode(Integer reviewCode); // 리뷰 코드로 특정 리뷰 조회
+    List<ReviewDTO> getReviewsByWriterCode(Integer writerCode); // 작성자 코드로 리뷰 조회
+    void insertReview(ReviewDTO review); // 리뷰 생성
+    void updateReview(ReviewDTO review); // 리뷰 수정
+    void deleteReview(Integer reviewCode); // 리뷰 삭제
 }
