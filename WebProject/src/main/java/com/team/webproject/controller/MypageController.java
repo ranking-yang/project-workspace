42335a48beaa783c90f27dc4a46b3ffc21c0ec6d
siese <siese@192.168.35.95>
2023-08-06 19:15:08 +0900

    230806 김수진 마이페이지 UI 완성

--- WebProject/src/main/java/com/team/webproject/controller/MypageController.java
+++ WebProject/src/main/java/com/team/webproject/controller/MypageController.java
@@ -1,27 +1,15 @@
 package com.team.webproject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.team.webproject.service.MypageService;
-
 @Controller
 public class MypageController {
-	
-	@Autowired
-	MypageService mapageService;
 	
 	// 메인 겸 예매내역
 	@GetMapping("/mypage")
 	String gotoMypage(Model model) {
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		String userID = ((UserDetails) principal).getUsername();
-		
-		model.addAttribute("tickets", mapageService.getAllTickets(userID));
 		
 		return "/mypage/mypage-ticket";
 	}
