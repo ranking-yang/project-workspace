72ce36f8760b42dc176af8c5de94e845d3022aa4
colamom1017 <132882291+colamom1017@users.noreply.github.com>
2023-08-09 14:47:43 +0900

    230809 이희정 커밋
    임시 커밋입니다 ..

--- WebProject/src/main/webapp/resources/product-list/js/product-list.js
+++ WebProject/src/main/webapp/resources/product-list/js/product-list.js
@@ -40,33 +40,33 @@ function toggleLike(event, buttonId, member_code) {
   };
 
 $(document).ready(function(){
-	$('.product-module').on('click', function(){
-		console.log($(this).data('pk'));
-		
-		if ($(this).data('category') !== "art") {
-	    		location.href = '../product-detail?performance_code=' + $(this).data('pk');				
-			} else {
-				location.href = '../product-detail-ex?performance_code=' + $(this).data('pk');				
-			}
-		
-	})
-});
-
-$(document).ready(function(){
-    // 페이지 로드 시 실행
     let urlParams = new URLSearchParams(window.location.search);
     let areaCode = urlParams.get('area_code');
-    
+
+    function redirectToDetailPage(performanceCode, category) {
+        let detailPageUrl = `../product-detail${category === "art" ? "-ex" : ""}`;
+        location.href = `${detailPageUrl}?performance_code=${performanceCode}`;
+    }
+
+    $('.product-module').on('click', function(){
+        let performanceCode = $(this).data('pk');
+        let category = $(this).data('category');
+
+        redirectToDetailPage(performanceCode, category);
+        
+    });
+
     if (areaCode) {
         $('.areaBtn[data-area="' + areaCode + '"]').addClass('selected');
     }
 
-    // 버튼 클릭 시 페이지 리다이렉트만 처리
     $('.areaBtn').on('click', function(){
         let area = $(this).data('area');
         location.href = '../product/area?area_code=' + area;
     });
 });
+
+
 
 $('.price').each(function(){ // 가격 표시 , 세자리마다 콤마 찍기
 		  var price = $(this).text();
